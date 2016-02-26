f5d3ac3db2a6247cd2816ba555daa58ab1abddb0
lukerobery <lrobery@hotmail.co.uk>
2016-02-26 14:29:38 +0000

    Updated to allow the user to logout fully
    Updated to allow the user to logout fully

--- js/google.js
+++ js/google.js
@@ -1,3 +1,8 @@
+ //Login Function -- Takes the user and then logs them in and redirects them to the charts
  function onSignIn(googleUser) {
     window.location.assign("charts.html");
  }
+ //Sign out function -- Basically redirects the user to the logout page of google then logs them out. Then they get redirected back to the hompage
+function signOut() {
+window.location="https://www.google.com/accounts/Logout?continue=https://appengine.google.com/_ah/logout?continue=http://insewincycharter.com/index.html";
+  }
